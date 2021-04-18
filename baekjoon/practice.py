0005ee9ba3fa1f35e5d90938b83ddc05ea1123d5
heyjaekim <heyjaekim@gmail.com>
2021-04-18 12:47:32 +0900

    feat(Jaeweon-Kim): [boj, 2544] 사회망 서비스

--- baekjoon/practice.py
+++ baekjoon/practice.py
@@ -1,32 +1,37 @@
 from collections import defaultdict
 import heapq
 
-def solve():
-    queue = [(constrcut_time[i], i) for i in range(1, len(precede_list)) if precede_list[i] == 0]
+def find_time(precede_list, building_next, construct_time, target):
+    queue = [(construct_time[i], i) for i in range(1, len(precede_list)) if precede_list[i] == 0]
     heapq.heapify(queue)
 
     while queue:
         finish_time, building = heapq.heappop(queue)
+
         if building == target:
             return finish_time
-        
-        for next_building in build_next[building]:
-            precede_list[next_building] -= 1
-            if precede_list[next_building] == 0:
-                heapq.heappush(queue, (finish_time + constrcut_time[next_building], next_building))
+
+        for next_build in building_next[building]:
+            precede_list[next_build] -= 1
+
+            if precede_list[next_build] == 0:
+                heapq.heappush(queue, (finish_time + construct_time[next_build], next_build))
+
 
 T = int(input())
 for i in range(T):
     N, K = map(int, input().split())
-    precede_list = [0] * (N+1)
+    precede_list = [0 for _ in range(N+1)]
     build_next = defaultdict(list)
-    constrcut_time = list(map(int, input().split()))
-    constrcut_time.insert(0, 0)
+    construct_time = list(map(int, input().split()))
+    construct_time.insert(0, 0)
 
-    for i in range(K):
+    for _ in range(K):
         a, b = map(int, input().split())
         precede_list[b] += 1
         build_next[a].append(b)
-    
+
     target = int(input())
-    print(solve())
+    print(find_time(precede_list, build_next, construct_time, target))
+
+    
