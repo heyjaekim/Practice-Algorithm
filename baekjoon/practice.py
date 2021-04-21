81a270cf20aaf6d8b26c7a69c7c073ab2d37e0e8
Jaeweon Kim <heyjaekim@gmail.com>
2021-04-21 14:45:22 +0900

    feat(Jaeweon-Kim): [boj, practice] Puyo Puyo

--- baekjoon/practice.py
+++ baekjoon/practice.py
@@ -1,49 +1,63 @@
 
-def possible(n):
+from collections import deque
 
-    if n == 15:
-        flag = True
-        for score in team_list:
-            if score[0] != 0 or score[1] != 0 or score[2] != 0:
-                flag = False
-                break
-        return flag
-    
+dx = [0, -1, 0, 1]
+dy = [-1, 0, 1, 0]
+
+def bfs(x, y, flag):
+    q = deque()
+    q.append([x, y])
+    cnt = 1
+    visited = [[0]*6 for _ in range(12)]
+    visited[x][y] = 1
+    while q:
+        pos_x, pos_y = q.popleft()
+        for i in range(4):
+            nx, ny = pos_x + dx[i], pos_y + dy[i]
+            if 0 <= nx < 12 and 0 <= ny < 6:
+                if not visited[nx][ny] and board[nx][ny] == board[pos_x][pos_y]:
+                    cnt += 1
+                    visited[nx][ny] = 1
+                    q.append([nx, ny])
+
+    if cnt >= 4:
+        flag += 1
+        for i in range(12):
+            for j in range(6):
+                if visited[i][j] == 1:
+                    board[i][j] = '.'
+
+    return flag
+
+def check_fall():
+    for i in range(10, -1, -1):
+        for j in range(6):
+            if board[i][j] != '.' and board[i+1][j] == '.':
+                for k in range(i+1, 12):
+                    if k == 11 and board[k][j] == '.':
+                        board[k][j] = board[i][j]
+                    elif board[k][j] != '.':
+                        board[k-1][j] = board[i][j]
+                        break
+                board[i][j] = '.'
+
+
+board = []
+for _ in range(12):
+    board.append(list(input()))
+
+ans = 0
+while True:
+    cnt = 0
+    for i in range(12):
+        for j in range(6):
+            if board[i][j] != '.':
+                cnt = bfs(i, j, cnt)
+        
+    if cnt == 0:
+        print(ans)
+        break
     else:
-        temp = False
-        for i in range(3):
-            team_1 = match_seq[n][0]
-            team_2 = match_seq[n][1]
-            if team_list[team_1][i] == 0 or team_list[team_2][2-i] == 0:
-                continue
+        ans += 1
 
-            team_list[team_1][i] -= 1
-            team_list[team_2][2-i] -= 1
-            temp = possible(n+1)
-            if temp: return True
-            team_list[team_1][i] += 1
-            team_list[team_2][2-i] += 1
-        return False
-
-
-
-
-ans = []
-match_seq = []
-for i in range(5):
-    for j in range(i+1, 6):
-        match_seq.append((i,j))
-
-for _ in range(4):
-    records = list(map(int, input().split()))
-    team_list = []
-
-    for i in range(6):
-        team_list.append(records[i*3 : i*3 + 3])
-    
-    if possible(0):
-        ans.append(1)
-    else:
-        ans.append(0)
-
-print(*ans)
+    check_fall()
