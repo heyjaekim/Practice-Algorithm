3f4c63facaf36b1f8475df45e68c2fcf536e5f58
Jaeweon Kim <heyjaekim@gmail.com>
2021-04-22 18:53:07 +0900

    feat(Jaeweon-Kim) [boj, 13460] 구슬 탈추2 (수정)

--- baekjoon/boj_13460.py
+++ baekjoon/boj_13460.py
@@ -7,23 +7,23 @@ dx = [0, -1, 0, 1]
 dy = [-1, 0, 1, 0]
 
 q = deque()
-board = [list(input()) for _ in range(N)]
+B = [list(input()) for _ in range(N)]
 visited = [[[[0] * (M) for _ in range(N)] for _ in range(M)] for _ in range(N)]
 
 def pos_init():
-    rx = ry = bx = by = 0
+    rx, ry, bx, by = 0, 0, 0, 0
     for i in range(N):
         for j in range(M):
-            if board[i][j] == 'R':
+            if B[i][j] == 'R':
                 rx, ry = i, j
-            elif board[i][j] == 'B':
+            elif B[i][j] == 'B':
                 bx, by = i, j
     q.append((rx, ry, bx, by, 1))
     visited[rx][ry][bx][by] = 1
 
 def move(x, y, dx, dy):
     cnt = 0
-    while board[x+dx][y+dy] != '#' and board[x][y] != 'O':
+    while B[x+dx][y+dy] != '#' and B[x][y] != 'O':
         x += dx
         y += dy
         cnt += 1
@@ -38,22 +38,22 @@ def bfs():
         for i in range(4):
             nrx, nry, rcnt = move(rx, ry, dx[i], dy[i])
             nbx, nby, bcnt = move(bx, by, dx[i], dy[i])
-            if board[nbx][nby] != 'O':
-                if board[nrx][nry] == 'O':
+            if B[nbx][nby] != 'O':
+                if B[nrx][nry] == 'O':
                     print(depth)
                     return
             
-            if nrx == nbx and nry == nby:
-                if rcnt > bcnt:
-                    nrx -= dx[i]
-                    nry -= dy[i]
-                else:
-                    nbx -= dx[i]
-                    nby -= dy[i]
-            
-            if not visited[nrx][nry][nbx][nby]:
-                visited[nrx][nry][nbx][nby] = 1
-                q.append((nrx, nry, nbx, nby, depth + 1))
+                if nrx == nbx and nry == nby:
+                    if rcnt > bcnt:
+                        nrx -= dx[i]
+                        nry -= dy[i]
+                    else:
+                        nbx -= dx[i]
+                        nby -= dy[i]
+                
+                if not visited[nrx][nry][nbx][nby]:
+                    visited[nrx][nry][nbx][nby] = 1
+                    q.append((nrx, nry, nbx, nby, depth + 1))
 
     print(-1)        
 
