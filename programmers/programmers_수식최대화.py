f61ec47013db277d8dbc5c6a76702f32a718816f
Jaeweon Kim <heyjaekim@gmail.com>
2021-05-07 19:29:28 +0900

    feat(Jaeweon-Kim): programmers, 수식 최대화

---
+++ programmers/programmers_수식최대화.py
@@ -0,0 +1,31 @@
+# 수식 최대화
+
+def calc(priority, n, expression):
+    if n == 2:
+        return str(eval(expression))
+    if priority[n] == '*':
+        res = eval('*'.join([calc(priority, n+1, e) for e in expression.split('*')]))
+    if priority[n] == '+':
+        res = eval('+'.join([calc(priority, n+1, e) for e in expression.split('+')]))
+    if priority[n] == '-':
+        res = eval('-'.join([calc(priority, n+1, e) for e in expression.split('-')]))
+    return str(res)
+
+def solution(expression):
+    answer = 0
+    priorities = [
+        ('*', '-', '+'),
+        ('*', '+', '-'),
+        ('+', '*', '-'),
+        ('+', '-', '*'),
+        ('-', '*', '+'),
+        ('-', '+', '*')
+    ]
+    for priority in priorities:
+        res = int(calc(priority, 0, expression))
+        answer = max(answer, abs(res))
+
+    return answer
+
+expression = "100-200*300-500+20"
+print(solution(expression))
